098cf5c0c8a523840af67635d9f04d8ea92d1ca2
Phil Maurer <philmau@gmail.com>
2025-11-22 17:38:12 +0000

    Don't run Sentry locally

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,13 @@ import * as Sentry from '@sentry/react'
 import App from './App.tsx'
 import '~/css/main.css'
 
-Sentry.init({
-	dsn: 'https://534366c7cbb145b6a078617778401236@o252569.ingest.sentry.io/5566300',
-	environment: import.meta.env.MODE,
-	release: `huely@${APP_VERSION}`,
-})
-
-console.log('APP_VERSION:', APP_VERSION)
+if (import.meta.env.MODE !== 'development') {
+	Sentry.init({
+		dsn: 'https://534366c7cbb145b6a078617778401236@o252569.ingest.sentry.io/5566300',
+		environment: import.meta.env.MODE,
+		release: `huely@${APP_VERSION}`,
+	})
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
